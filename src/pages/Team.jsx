d324c26e9c2cdb1c72b1defaa83f59bf5145bc2e
chhengz <sokchheng802@gmail.com>
2025-04-04 20:37:09 +0700

    .

--- src/pages/Team.jsx
+++ src/pages/Team.jsx
@@ -55,7 +55,7 @@ const Team = () => {
             <rect
               width="100%"
               height="100%"
-              stroke-width="0"
+              strokeWidth="0"
               fill="url(#e9033f3e-f665-41a6-84ef-756f6778e6fe)"
             ></rect>
           </svg>
